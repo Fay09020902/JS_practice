dd9a4ac9ea68de9024d6aa0196416b88522b065e
yinjiao <fyj121322@gmail.com>
2024-12-20 16:07:13 -0800

    ad

--- JSTutorial/NeetCodeJs.js
+++ JSTutorial/NeetCodeJs.js
@@ -146,3 +146,93 @@ var timeLimit = function(fn, t) {
         });
     };
 };
+
+//2636. Promise Pool
+//wrong my approach
+var promisePool = async function(functions, n) {
+    let queue = []
+    let index_next = 0
+    for (let i = 0; i < n && index_next < functions.length; i++) {
+        let promise = functions[index_next]()
+        index_next += 1
+        queue.push(promise)
+        //if promise in the queue get resolved, then move it out from queue
+        promise.then(() => queue.splice(queue.indexOf(promise), 1))
+    }
+
+    while (queue.length > 0 || index_next < functions.length) {
+        if (queue.length < n && index_next <   functions.length) {
+            let promise = functions[index_next++]();
+            queue.push(promise);
+
+            // Remove resolved promises from the queue
+            promise.then(() => queue.splice(queue.indexOf(promise), 1));
+        }
+
+    }
+    //cannot make sure all the pending promises got resolved
+    return Promise.resolve()
+};
+
+//2627. Debounce
+var debounce = function(fn, t) {
+    //call first one, and if next call called within t from the first call, cancel the first call, otherwise keep first one and call second one
+    let id
+    return function(...args) {
+        clearTimeout(id) //if alreay been executed, this would not do anything
+        id = setTimeout(() => fn(...args), t);
+    }
+};
+
+
+
+//2628. JSON Deep Equal
+var areDeeplyEqual = function(o1, o2) {
+    if(o1 === null || o2 === null) {
+        return o1 === o2
+    }
+    if((typeof o1) !== (typeof o2)) {
+        return false
+    }
+    if (typeof o1 !== 'object') {
+        return o1 === o2
+    }
+    if(Array.isArray(o1) && Array.isArray(o2)) {
+        if (o1.length !== o2.length) {
+            return false;
+        }
+
+        for (let i = 0; i < o1.length; i++) {
+            if (!areDeeplyEqual(o1[i], o2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (typeof o1 === "object"  && typeof o2 === "object") {
+        if (Array.isArray(o1) || Array.isArray(o2)) {
+            return false;
+        }
+        const keys1 = Object.keys(o1);
+        const keys2 = Object.keys(o2);
+        if (keys1.length !== keys2.length) {
+            return false;
+        }
+        for (let key of keys1) {
+            if (!keys2.includes(key) || !areDeeplyEqual(o1[key], o2[key])) {
+                return false;
+            }
+        }
+        return true
+        //wrong  you need to ensure that all key-value pairs are checked and accumulated before returning the final result. This can be done by using a flag or returning false only if a mismatch is found.
+
+        // for (let key of keys1) {
+        //     if (keys2.includes(key)) {
+        //         return areDeeplyEqual(o1[key], o2[key])
+        //     } else {
+        //         return false
+        //     }
+        // }
+    }
+};
