b2a332baa1e4a1e7f5c35622c51b667490be3089
yinjiao <fyj121322@gmail.com>
2025-01-17 13:10:49 -0800

    ad

--- JSTutorial/Easy.js
+++ JSTutorial/Easy.js
@@ -490,3 +490,26 @@
 
 //     return result;
 // };
+
+
+//271， encode decode
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function(strs) {
+    let encode_message = ''
+    return strs.join("π")
+};
+
+/**
+ * Decodes a single string to a list of strings.
+ *
+ * @param {string} s
+ * @return {string[]}
+ */
+var decode = function(s) {
+    return s.split("π")
+};
