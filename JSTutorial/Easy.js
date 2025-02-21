2e3bb78c27020bbb9757c93f89ee430953c70f0b
yinjiao <fyj121322@gmail.com>
2025-02-21 13:45:28 -0800

    add

--- JSTutorial/Easy.js
+++ JSTutorial/Easy.js
@@ -1242,3 +1242,16 @@
 
 //     return dfs(root.left, root.right);
 // };
+
+
+// //111
+// var minDepth = function(root) {
+//     if (root === null) return 0;
+//     if(root.left === null) {
+//         return minDepth(root.right) + 1
+//     }
+//     if(root.right === null) {
+//         return minDepth(root.left) + 1
+//     }
+//     return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+// };
